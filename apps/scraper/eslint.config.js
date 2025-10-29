9c41491242dc1f360391b8ea851caed414e9f9fe
Edonmorinaa <148013393+Edonmorinaa@users.noreply.github.com>
2025-10-29 22:35:28 +0100

    deployment fix for dashboard

---
+++ apps/scraper/eslint.config.js
@@ -0,0 +1,36 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-console': 'off',
+      'no-undef': 'off',
+      'prettier/prettier': 'warn',
+      ...prettier.rules,
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', '*.js', '*.config.js'],
+  },
+];
+
